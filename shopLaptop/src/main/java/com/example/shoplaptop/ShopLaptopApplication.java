e8cc01fb2d5f67fe507141d9f10e641ea4737101
ducnguyen17 <134828025+ducnguyen17@users.noreply.github.com>
2023-07-28 14:37:56 +0700

    Create ShopLaptopApplication.java

---
+++ shopLaptop/src/main/java/com/example/shoplaptop/ShopLaptopApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoplaptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLaptopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopLaptopApplication.class, args);
+    }
+
+}
